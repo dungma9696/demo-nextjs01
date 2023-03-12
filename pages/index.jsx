51c7b88d7d0febe3c5bb6c21f4988f5cec5ef597
dungma <dungma9696@gmail.com>
2023-03-12 23:49:21 +0700

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,7 @@ import styles from '../styles/Home.module.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  console.log('');
   return (
     <>
       <Head>
